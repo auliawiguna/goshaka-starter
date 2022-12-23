4861d4a56b5eb247860ec7326d4ffc437b8891f1
Ahmad Aulia Wiguna <aulia@softwareseni.com>
2022-12-23 23:43:39 +0700

    chore:remove function

--- main.go
+++ main.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	apiRoutes "goshaka/app/routes"
 	appConfig "goshaka/configs"
 	appDatabase "goshaka/database"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 )
-
-func getEnv(key string) string {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatalf("Error Loading Env")
-	}
-
-	return os.Getenv(key)
-}
 
 func main() {
 	app := fiber.New()
