c40344e1a03c38be676b7c76716abb6598b5da6c
Ahmad Aulia Wiguna <aulia@softwareseni.com>
2022-12-26 08:03:12 +0700

    ref:implement middleware compressor

--- app/middlewares/default.go
+++ app/middlewares/default.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
@@ -10,5 +11,8 @@ func DefaultMiddleware(a *fiber.App) {
 	a.Use(
 		cors.New(),
 		logger.New(),
+		compress.New(compress.Config{
+			Level: compress.LevelBestSpeed,
+		}),
 	)
 }
