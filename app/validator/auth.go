d713403d601775ab9bbdd2e598e2d4b6c6baed96
Ahmad Aulia Wiguna <aulia@softwareseni.com>
2023-02-05 16:02:42 +0700

    feat: login using google one tap

--- app/validator/auth.go
+++ app/validator/auth.go
@@ -70,3 +70,24 @@ func ResetPasswordValidator(c *fiber.Ctx) error {
 
 	return c.Next()
 }
+
+func GoogleOneTap(c *fiber.Ctx) error {
+	var errors []*structs.IError
+	body := new(structs.GoogleOneTap)
+	c.BodyParser(&body)
+
+	err := Validator.Struct(body)
+
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			var el structs.IError
+			el.Field = err.Field()
+			el.Tag = err.Tag()
+			el.Value = err.Param()
+			errors = append(errors, &el)
+		}
+		return helpers.UnprocessableResponse(c, errors, "unprocessable entity")
+	}
+
+	return c.Next()
+}
---
+++ helpers/google.go
@@ -0,0 +1,59 @@
+package helpers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"goshaka/configs"
+
+	"google.golang.org/api/idtoken"
+)
+
+type IdentityToolkitTokenInfo struct {
+	Sub           string `json:"sub"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	Locale        string `json:"locale"`
+	Aud           string `json:"aud"`
+	Iss           string `json:"iss"`
+}
+
+func VerifyIdToken(ctx context.Context, idToken string) (*IdentityToolkitTokenInfo, error) {
+	var googleClientId string = configs.GetEnv("GOOGLE_CLIENT_ID") // from credentials in the Google dev console
+	var info IdentityToolkitTokenInfo
+
+	tokenValidator, err := idtoken.NewValidator(context.Background())
+	if err != nil {
+		return &info, fmt.Errorf("cannot assign new validator")
+	}
+
+	payload, err := tokenValidator.Validate(context.Background(), idToken, googleClientId)
+	if err != nil {
+		return &info, fmt.Errorf("cannot validate the google token")
+	}
+
+	// Marshaling the map into JSON
+	b, err := json.Marshal(payload.Claims)
+	if err != nil {
+		return &info, fmt.Errorf("cannot marshal the google token")
+	}
+
+	// Unmarshaling the JSON into the struct
+	if err := json.Unmarshal(b, &info); err != nil {
+		return &info, fmt.Errorf("cannot unmarshal the google token")
+	}
+
+	if info.Aud != configs.GetEnv("GOOGLE_CLIENT_ID") {
+		return &info, fmt.Errorf("invalid client ID")
+	}
+
+	if info.Iss != "accounts.google.com" && info.Iss != "https://accounts.google.com" {
+		return &info, fmt.Errorf("invalid iss")
+	}
+
+	return &info, nil
+}
