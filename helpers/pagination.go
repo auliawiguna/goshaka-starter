2733cf504c74332b38ad5b33a0834c22cbd4f6e2
Ahmad Aulia Wiguna <aulia@softwareseni.com>
2023-02-07 13:58:28 +0700

    chore: lint

--- helpers/pagination.go
+++ helpers/pagination.go
@@ -1,9 +1,9 @@
 package helpers
 
 type Pagination struct {
-	Limit      int         `json:"limit,omitempty;query:limit"`
-	Page       int         `json:"page,omitempty;query:page"`
-	Sort       string      `json:"sort,omitempty;query:sort"`
+	Limit      int         `json:"limit"`
+	Page       int         `json:"page"`
+	Sort       string      `json:"sort"`
 	TotalRows  int64       `json:"total_rows"`
 	TotalPages int         `json:"total_pages"`
 	Rows       interface{} `json:"rows"`
