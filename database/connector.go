80e7e0f2c21ac621fda47092e141112976aa5407
Ahmad Aulia Wiguna <aulia@softwareseni.com>
2023-02-16 08:46:27 +0700

    ref: add database logger

--- database/connector.go
+++ database/connector.go
@@ -11,6 +11,7 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -19,17 +20,24 @@ func Connect() error {
 	var err error
 	dbConnURL, _ := appHelper.ConnectionURLBuilder(appConfig.GetEnv("DB_DRIVER"))
 
+	var loggerInfo logger.Interface
+
+	if appConfig.GetEnv("ENV") == "dev" {
+		loggerInfo = logger.Default.LogMode(logger.Info)
+	}
 	switch appConfig.GetEnv("DB_DRIVER") {
 	case "mysql":
 		DB, err = gorm.Open(mysql.Open(dbConnURL), &gorm.Config{
 			SkipDefaultTransaction:                   true,
 			PrepareStmt:                              true,
 			DisableForeignKeyConstraintWhenMigrating: true,
+			Logger:                                   loggerInfo,
 		})
 	case "postgres":
 		DB, err = gorm.Open(postgres.Open(dbConnURL), &gorm.Config{
 			SkipDefaultTransaction: true,
 			PrepareStmt:            true,
+			Logger:                 loggerInfo,
 		})
 	}
 
