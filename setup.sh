4ce2f02a755caa88c63f628d88775c7e2c7cd11f
Ahmad Aulia Wiguna <aulia@softwareseni.com>
2023-02-20 16:26:19 +0700

    build: add docs

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 docker-compose -f docker-compose.yml --env-file .docker-env down 
 docker-compose -f docker-compose.yml --env-file .docker-env up -d 
 
+# Start Supervisord
+# docker exec -it goshaka_be /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+
 # Restart supervisord
 # docker exec -it goshaka_be supervisorctl restart goshaka-worker:
 
