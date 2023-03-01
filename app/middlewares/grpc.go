8682c416cb378d5df07d48157658796350dc4b24
Ahmad Aulia Wiguna <aulia@softwareseni.com>
2023-03-01 08:36:16 +0700

    wip: grpc

---
+++ app/middlewares/grpc.go
@@ -0,0 +1,78 @@
+package middlewares
+
+import (
+	"goshaka/helpers"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+)
+
+// HTTP Throttle by key
+//
+//	param	key string
+//	param	max int
+//	param	sec int
+//	receiver c *fiber.Ctx
+//	return error
+func ThrottleByKey(key string, max, sec int) func(c *fiber.Ctx) error {
+	return limiter.New(limiter.Config{
+		// Next: func(c *fiber.Ctx) bool {
+		// return c.IP() == "127.0.0.1"
+		// },
+		Max:        max,
+		Expiration: time.Duration(sec) * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return key
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return helpers.TooManyRequestResponse(c)
+		},
+	})
+}
+
+// HTTP Throttle by key and user's IP address
+//
+//	param	key string
+//	param	max int
+//	param	sec int
+//	receiver c *fiber.Ctx
+//	return error
+func ThrottleByKeyAndIP(key string, max, sec int) func(c *fiber.Ctx) error {
+	return limiter.New(limiter.Config{
+		// Next: func(c *fiber.Ctx) bool {
+		// return c.IP() == "127.0.0.1"
+		// },
+		Max:        max,
+		Expiration: time.Duration(sec) * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return string(c.IP()) + key
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return helpers.TooManyRequestResponse(c)
+		},
+	})
+}
+
+// HTTP Throttle by user's IP address
+//
+//	param	key string
+//	param	max int
+//	param	sec int
+//	receiver c *fiber.Ctx
+//	return error
+func ThrottleByIp(max, sec int) func(c *fiber.Ctx) error {
+	return limiter.New(limiter.Config{
+		// Next: func(c *fiber.Ctx) bool {
+		// return c.IP() == "127.0.0.1"
+		// },
+		Max:        max,
+		Expiration: time.Duration(sec) * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP()
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return helpers.TooManyRequestResponse(c)
+		},
+	})
+}
--- go.mod
+++ go.mod
@@ -24,7 +24,7 @@ require (
 )
 
 require (
-	cloud.google.com/go/compute v1.14.0 // indirect
+	cloud.google.com/go/compute v1.15.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
@@ -76,8 +76,8 @@ require (
 	golang.org/x/text v0.6.0 // indirect
 	golang.org/x/tools v0.5.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20221227171554-f9683d7f8bef // indirect
-	google.golang.org/grpc v1.51.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
